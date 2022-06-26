f67196dc2e4fb0c27d7c0113d4001fd66f8e73c4
Patrick Brennan <patbrennan7@gmail.com>
2022-06-26 17:05:32 -0400

    Code Cleaned Up

--- myproject/main.js
+++ myproject/main.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './style.css';
 
 import * as THREE from 'three';
 
@@ -6,10 +6,15 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+	canvas: document.querySelector('#bg'),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -22,9 +27,9 @@ const geometry = new THREE.TorusGeometry(15, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial({ color: 0xfcba03 }); //? "0x" is a hexidecimal literal
 const torus = new THREE.Mesh(geometry, material);
 
-scene.add(torus)
+scene.add(torus);
 
-const pointLight = new THREE.PointLight(0xffffff)
+const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
@@ -34,13 +39,13 @@ scene.add(pointLight, ambientLight);
 renderer.render(scene, camera);
 
 function animate() {
-  requestAnimationFrame(animate);
+	requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
+	torus.rotation.x += 0.01;
+	torus.rotation.y += 0.005;
+	torus.rotation.z += 0.01;
 
-  renderer.render(scene, camera);
+	renderer.render(scene, camera);
 }
 
-animate()
+animate();
