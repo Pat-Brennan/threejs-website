016abe1fc5bec1cb383d5323a2874af50439d924
Patrick Brennan <patbrennan7@gmail.com>
2022-06-27 11:47:18 -0400

    Cosmos Created/Logo Mapped to Cube

--- myproject/main.js
+++ myproject/main.js
@@ -2,6 +2,8 @@ import './style.css';
 
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 //? You will always need a SCENE, CAMERA, and RENDERER
 
 const scene = new THREE.Scene();
@@ -33,8 +35,39 @@ const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
-
 scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight); //? Shows the exact location of pointlight
+const gridHelper = new THREE.GridHelper(200, 50); //? Creates a grid to get a feel for the space
+scene.add(lightHelper, gridHelper);
+
+//? listens for dom events on the mouse and updates camera position accordingly
+const controls = new OrbitControls(camera, renderer.domElement); 
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star)
+}
+
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load('Cosmos.jpg');
+scene.background = spaceTexture;
+
+const logoTexture = new THREE.TextureLoader().load('logo.jpg');
+
+const logo = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+new THREE.MeshBasicMaterial({ map: logoTexture })
+)
+
+scene.add(logo)
 
 renderer.render(scene, camera);
 
@@ -43,7 +76,9 @@ function animate() {
 
 	torus.rotation.x += 0.01;
 	torus.rotation.y += 0.005;
-	torus.rotation.z += 0.01;
+  torus.rotation.z += 0.01;
+  
+  controls.update(); //? Ensures the last position of orbit stays true
 
 	renderer.render(scene, camera);
 }
