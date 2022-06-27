0914ed34803507bd3ddf2fa2dd5986e20945ad23
Patrick Brennan <patbrennan7@gmail.com>
2022-06-27 12:19:28 -0400

    Camera Scroll Added

--- myproject/main.js
+++ myproject/main.js
@@ -42,17 +42,21 @@ const gridHelper = new THREE.GridHelper(200, 50); //? Creates a grid to get a fe
 scene.add(lightHelper, gridHelper);
 
 //? listens for dom events on the mouse and updates camera position accordingly
-const controls = new OrbitControls(camera, renderer.domElement); 
+const controls = new OrbitControls(camera, renderer.domElement);
+
+//* Stars
 
 function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25);
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const star = new THREE.Mesh(geometry, material);
+	const geometry = new THREE.SphereGeometry(0.25);
+	const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	const star = new THREE.Mesh(geometry, material);
 
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+	const [x, y, z] = Array(3)
+		.fill()
+		.map(() => THREE.MathUtils.randFloatSpread(100));
 
-  star.position.set(x, y, z);
-  scene.add(star)
+	star.position.set(x, y, z);
+	scene.add(star);
 }
 
 Array(200).fill().forEach(addStar);
@@ -60,14 +64,51 @@ Array(200).fill().forEach(addStar);
 const spaceTexture = new THREE.TextureLoader().load('Cosmos.jpg');
 scene.background = spaceTexture;
 
+//* Logo
+
 const logoTexture = new THREE.TextureLoader().load('logo.jpg');
 
 const logo = new THREE.Mesh(
-  new THREE.BoxGeometry(3, 3, 3),
-new THREE.MeshBasicMaterial({ map: logoTexture })
-)
+	new THREE.BoxGeometry(3, 3, 3),
+	new THREE.MeshBasicMaterial({ map: logoTexture })
+);
 
-scene.add(logo)
+scene.add(logo);
+
+//* Moon
+
+const moonTexture = new THREE.TextureLoader().load('Moon.jpg');
+
+const moon = new THREE.Mesh(
+	new THREE.SphereGeometry(3, 32, 32),
+	new THREE.MeshStandardMaterial({
+		map: moonTexture,
+	})
+);
+
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+//* Moving the Camera
+
+function moveCamera() {
+
+	const t = document.body.getBoundingClientRect().top;
+	moon.rotation.x += 0.05;
+	moon.rotation.y += 0.75;
+	moon.rotation.z += 0.05;
+
+	logo.rotation.x += 0.01;
+	logo.rotation.z += 0.01;
+
+	camera.position.z = t * -0.01;
+	camera.position.x = t * -0.0002;
+	camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
 
 renderer.render(scene, camera);
 
@@ -76,9 +117,9 @@ function animate() {
 
 	torus.rotation.x += 0.01;
 	torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
-  
-  controls.update(); //? Ensures the last position of orbit stays true
+	torus.rotation.z += 0.01;
+
+	controls.update(); //? Ensures the last position of orbit stays true
 
 	renderer.render(scene, camera);
 }
